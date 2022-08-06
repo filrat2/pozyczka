18d0e109e6b3d7bf92d3f0cb4df3116ce51e819f
Filip Ratajszczak <ratajszczak.filip@gmail.com>
2022-08-06 19:04:29 +0200

    add final version of pierwszy_program_pozyczka.py

--- pierwszy_program_pozyczka.py
+++ pierwszy_program_pozyczka.py
@@ -1,20 +1,20 @@
-def wielkosc_zadluzenia(kwota_poczatkowa, wartosc_oprocentowania, wartosc_raty):
-    if type(kwota_poczatkowa) != int and type(kwota_poczatkowa) != float:
-        print("Kwota początkowa kredytu musi być liczbą.")
-    elif kwota_poczatkowa <= 0:
-        print("Kwota początkowa kredytu musi być większa niż 0.")
-    elif type(wartosc_oprocentowania) != int and type(wartosc_oprocentowania) != float:
-        print("Wartość oprocentowania musi być liczbą.")
-    elif type(wartosc_raty) != int and type(wartosc_raty) != float:
-        print("Wartość raty musi być liczbą.")
-    elif wartosc_raty <= 0:
-        print("Wartość raty kredytu musi być większa niż 0.")
-    else:
-        wielkosc_zadluzenia_zmienna = (1 + ((wartosc_oprocentowania + 3) / 12000)) * kwota_poczatkowa - wartosc_raty
-        print(wielkosc_zadluzenia_zmienna)
+# main function
 
+def wielkosc_zadluzenia(kwota_poczatkowa, wartosc_oprocentowania, wartosc_raty):
+        wielkosc_zadluzenia_zmienna = (1 + ((wartosc_oprocentowania + 3) / 1200)) \
+                                      * kwota_poczatkowa - wartosc_raty
+        diff = kwota_poczatkowa - wielkosc_zadluzenia_zmienna
+        print(f"Twoja pozostała kwota kredytu to "
+              f"{round(wielkosc_zadluzenia_zmienna, 2)}, "
+              f"to {round(diff, 2)} mniej niż w poprzednim miesiącu.")
+
+
+# define variables / user inputs
 kwota_poczatkowa = float(input("Wprowadź kwotę początkową kredytu: "))
-wartosc_oprocentowania = float(input("Wprowadź wartość oprocentowania: "))
+wartosc_oprocentowania = float(input("Wprowadź wartość oprocentowania ponad inflację: "))
 wartosc_raty = float(input("Wprowadź wysokość raty kredytu: "))
 
-variables_in_function = wielkosc_zadluzenia(kwota_poczatkowa, wartosc_oprocentowania, wartosc_raty)
+# connecting user inputs with function
+variables_in_function = wielkosc_zadluzenia(kwota_poczatkowa,
+                                            wartosc_oprocentowania,
+                                            wartosc_raty)
